6f42d91b29d66a7e1698a11096bd6b71533477ab
Lucas Frezzini <luquiitas.f@gmail.com>
2023-02-02 20:10:05 -0300

    Redesign: Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,24 @@
+import { Link } from 'react-router-dom';
+
 import './Footer.scss';
+import logo from '../../assets/img/logo-primary.png';
 
 function Footer () {
+
+    const y = new Date().getFullYear();
+
     return(
         <div className="container">
-            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <p className="col-md-4 mb-0 text-muted">© 2022 Company, Inc</p>
+            <footer className="Footer d-flex flex-wrap justify-content-between align-items-center">
+                <p className="col-md-4 mb-0">©{y} MOVIEMOVIE, Inc</p>
 
-                {/* <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-                <svg className="bi me-2" width="40" height="32"></svg>
-                </a> */}
+                {<Link to="/"><img src={logo} /></Link>}
 
                 <ul className="nav col-md-4 justify-content-end">
-                <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Home</a></li>
-                <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Features</a></li>
-                <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Pricing</a></li>
-                <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">FAQs</a></li>
-                <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">About</a></li>
+                    <li className="nav-item"><Link to="/home" className="nav-link px-2">Home</Link></li>
+                    <li className="nav-item"><Link to="/movies" className="nav-link px-2">Movies</Link></li>
+                    <li className="nav-item"><Link to="/about" className="nav-link px-2">About us</Link></li>
+                    <li className="nav-item"><Link to="/contact" className="nav-link px-2">Contact</Link></li>
                 </ul>
             </footer>
         </div>
