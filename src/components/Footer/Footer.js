d3017ce8aa86f974e4fc874529064cf8fd2cee73
Lucas Frezzini <luquiitas.f@gmail.com>
2023-02-02 23:38:14 -0300

    Fix: Correct urls in Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,7 +15,7 @@ function Footer () {
                 {<Link to="/"><img src={logo} /></Link>}
 
                 <ul className="nav col-md-4 justify-content-end">
-                    <li className="nav-item"><Link to="/home" className="nav-link px-2">Home</Link></li>
+                    <li className="nav-item"><Link to="/" className="nav-link px-2">Home</Link></li>
                     <li className="nav-item"><Link to="/movies" className="nav-link px-2">Movies</Link></li>
                     <li className="nav-item"><Link to="/about" className="nav-link px-2">About us</Link></li>
                     <li className="nav-item"><Link to="/contact" className="nav-link px-2">Contact</Link></li>
