eea14498bc17c36929e0840fc66b3a32193f2ad8
Lucas Frezzini <luquiitas.f@gmail.com>
2023-02-06 13:12:52 -0300

    Fix: use NavLink instead of Link en navigation menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import SearchForm from "../SearchForm/SearchForm";
 
 import './Header.scss';
@@ -15,10 +15,10 @@ function Header (props) {
     return (
         <nav className="navbar navbar-expand-lg fixed-top">
             <div className="container">
-                <Link className="navbar-brand" to="/">
+                <NavLink className="navbar-brand" to="/">
                     <img src={logo} alt="Logo site" />
                     CinePLUS
-                </Link>
+                </NavLink>
                 <button onClick={toggleMenu} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-three-dots" viewBox="0 0 16 16">
                         <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
@@ -27,17 +27,17 @@ function Header (props) {
                 <div className="collapse navbar-collapse justify-content-between  " id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <Link className="nav-link" onClick={toggleMenu} to="/">Home</Link>
+                            <NavLink className={({isActive}) => (isActive ? "nav-link active" : "nav-link")} onClick={toggleMenu} to="/">Home</NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" onClick={toggleMenu} to="/movies">Movies</Link>
+                            <NavLink className={({isActive}) => (isActive ? "nav-link active" : "nav-link")} onClick={toggleMenu} to="/movies">Movies</NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" onClick={toggleMenu} to="/about">About us</Link>
+                            <NavLink className={({isActive}) => (isActive ? "nav-link active" : "nav-link")} onClick={toggleMenu} to="/about">About us</NavLink>
                         </li>
                         { favourites.length > 0 &&
                         <li className="nav-item">
-                            <Link className="nav-link" onClick={toggleMenu} to="/favourites">Favourites</Link>
+                            <NavLink className={({isActive}) => (isActive ? "nav-link active" : "nav-link")} onClick={toggleMenu} to="/favourites">Favourites</NavLink>
                             <span className="badge">{favourites.length}</span>
                         </li>
                         }
